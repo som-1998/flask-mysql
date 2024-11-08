6586601fbc1ff7afefea25572666edb4b9ab95cc
Somnath Chakraborty <115790983+som-1998@users.noreply.github.com>
2024-11-08 18:54:50 +0530

    Update config.py

--- project/config.py
+++ project/config.py
@@ -3,6 +3,6 @@
 class Config:
     MYSQL_HOST = 'localhost'
     MYSQL_USER = 'root'
-    MYSQL_PASSWORD = 'Ams_12345'
+    MYSQL_PASSWORD = '********'
     MYSQL_DB = 'Student_db'
-    SECRET_KEY = 'abcdefgh'
+    SECRET_KEY = '**********'
